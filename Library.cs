bb3e25fcf5c2ecfae91baa9f69493923627f44ba
Matej Durana <matejdurana0@gmail.com>
2024-03-17 23:26:30 +0100

    pridanie dedicnosti, ktora porusuje Substituční princip Liskové, tak, ze meni spravanie zdedenej funkcie

--- Library.cs
+++ Library.cs
@@ -1,47 +1,41 @@
-﻿using System;
+﻿using SWI2_DUR0133;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SWI2_DUR0133
+public class Library
 {
-    public class Library
+    public List<Book> Books { get; set; }
+
+    public Library()
     {
-        public List<Book> Books { get; set; }
+        Books = new List<Book>();
+    }
 
-        public Library()
+    public virtual void CheckOutBook(Book book, Patron patron)
+    {
+        if (book.IsAvailable)
         {
-            Books = new List<Book>();
+            Console.WriteLine($"{book.Title} has been checked out by {patron.Name}");
+            book.IsAvailable = false;
+            patron.BooksCheckedOut.Add(book);
         }
-
-        public void CheckOutBook(Book book, Patron patron)
+        else
         {
-            if (book.IsAvailable)
-            {
-                book.IsAvailable = false;
-                patron.BooksCheckedOut.Add(book);
-                Console.WriteLine($"{book.Title} has been checked out by {patron.Name}");
-            }
-            else
-            {
-                Console.WriteLine($"{book.Title} is not available for checkout");
-            }
-        }
-
-        public void ReturnBook(Book book, Patron patron)
-        {
-            if (patron.BooksCheckedOut.Contains(book))
-            {
-                patron.BooksCheckedOut.Remove(book);
-                book.IsAvailable = true;
-                Console.WriteLine($"{book.Title} has been returned by {patron.Name}");
-            }
-            else
-            {
-                Console.WriteLine($"{patron.Name} does not have {book.Title} checked out");
-            }
+            Console.WriteLine($"{book.Title} is not available for checkout");
         }
     }
 
+    public void ReturnBook(Book book, Patron patron)
+    {
+        if (patron.BooksCheckedOut.Contains(book))
+        {
+            Console.WriteLine($"{book.Title} has been returned by {patron.Name}");
+            patron.BooksCheckedOut.Remove(book);
+            book.IsAvailable = true;
+        }
+        else
+        {
+            Console.WriteLine($"{patron.Name} does not have {book.Title} checked out");
+        }
+    }
 }
---
+++ RestrictedLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWI2_DUR0133
+{
+    public class RestrictedLibrary : Library
+    {
+        public override void CheckOutBook(Book book, Patron patron)
+        {
+            Console.WriteLine($"{patron.Name} is trying to check out {book.Title} from a restricted library.");
+            if (book.IsAvailable)
+            {
+                base.CheckOutBook(book, patron);
+            }
+            else
+            {
+                Console.WriteLine($"{book.Title} is not available for checkout at this time");
+            }
+        }
+    }
+}
